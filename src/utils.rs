74d210a24367678ad2883ac2d1ecb56fda25c23b
c2fc2f <contact@c2fc2f.com>
2026-04-01 15:23:03 +0200

    feat(src/utils): Use a strict Regex for a float & LazyLock to init it

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,22 @@
-use std::str::FromStr;
+use std::{str::FromStr, sync::LazyLock};
 
 use regex::Regex;
 
+static FLOAT_RE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"\d+\.\d+").expect("Error during the regex creation")
+});
+
 /// Finds the first float in a string and parses it into a generic type T.
-pub fn find_first_float<T>(text: &str) -> Option<T>
-where
-    T: FromStr,
-{
-    Regex::new(r"[-+]?\d*\.?\d+([eE][-+]?\d+)?")
-        .expect("Error during the regex creation")
+pub fn find_first_float<T: FromStr>(text: &str) -> Option<T> {
+    FLOAT_RE
         .find(text)
+        .and_then(|m| m.as_str().parse::<T>().ok())
+}
+
+/// Finds the last float in a string and parses it into a generic type T.
+pub fn find_last_float<T: FromStr>(text: &str) -> Option<T> {
+    FLOAT_RE
+        .find_iter(text)
+        .last()
         .and_then(|m| m.as_str().parse::<T>().ok())
 }
